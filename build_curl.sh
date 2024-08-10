dbd623189f6304854c66fad6e8528c2203113605
vyfor <92883017+vyfor@users.noreply.github.com>
2024-08-10 12:47:05 +0500

    Create build_curl.sh

---
+++ build_curl.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+VERSION=8.9.1
+
+cd ~
+sudo apt-get update -y
+sudo apt-get install -y nghttp2 libnghttp2-dev libssl-dev build-essential wget
+wget https://curl.haxx.se/download/curl-${VERSION}.tar.gz
+tar -xzvf curl-${VERSION}.tar.gz 
+rm -f curl-${VERSION}.tar.gz
+cd curl-${VERSION}
+./configure --prefix=/usr/local --with-ssl --with-nghttp2 --enable-websockets
+make -j4
+sudo make install
+sudo ldconfig
+cd ~
+rm -rf curl-${VERSION}
